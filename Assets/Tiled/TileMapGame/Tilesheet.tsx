61469ebaca8cb9c63b88ca4c5ab324f983e7f373
CripplingTonic <asad.malik@students.mq.edu.au>
2025-05-07 18:04:59 +1000

    Added Tilemap

---
+++ Assets/Tiled/TileMapGame/Tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Tilesheet" tilewidth="16" tileheight="16" spacing="1" tilecount="136" columns="17">
+ <image source="../kenney_monochrome-pirates/Default/Tilemap/tilemap.png" width="288" height="135"/>
+</tileset>
